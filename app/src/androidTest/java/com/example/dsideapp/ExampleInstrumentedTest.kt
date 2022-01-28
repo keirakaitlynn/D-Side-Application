bd4e5b7e16cd556e9f40d880f31790da0e5836c2
keira <keirakaitlynn@gmail.com>
2022-01-27 21:03:04 -0800

    Package name: dside -> dsideapp.

--- app/src/androidTest/java/com/example/dsideapp/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/dsideapp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.actualenvironment", appContext.packageName)
+        assertEquals("com.example.dsideapp", appContext.packageName)
     }
 }
