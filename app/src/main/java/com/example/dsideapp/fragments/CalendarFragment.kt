21f55caff6d7b2157b9990e2f3d71c522995fcf7
Victor Rodriguez <victor.rodriguez02@student.csulb.edu>
2022-02-04 19:53:17 -0800

    Adding Customization to the calendar :)

--- app/src/main/java/com/example/dsideapp/fragments/CalendarFragment.kt
+++ app/src/main/java/com/example/dsideapp/fragments/CalendarFragment.kt
@@ -4,14 +4,42 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CalendarView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.dsideapp.R
 
 class CalendarFragment : Fragment() {
+    private lateinit var viewOfLayout: View
+    var calendar: CalendarView? = null
+    private var dateView: TextView? = null
+
     override fun onCreateView(
+
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_calendar, container, false)
+    ): View {
+        viewOfLayout = inflater.inflate(R.layout.fragment_calendar, container, false)
+        // Variables for easy manipulation of objects in the activity_main.xml file   🙂
+        calendar = viewOfLayout.findViewById<View>(R.id.calendar) as CalendarView
+        dateView = viewOfLayout.findViewById<View>(R.id.date_view) as TextView
 
+        // Listener checks for a tap on a day
+        calendar!!
+            .setOnDateChangeListener {
+                // In here we can probably just make a popup / change page with a view of the day and stuff
+                // and query the DB to actually get the event info 🙂
+
+                _, year, month, dayOfMonth ->
+
+                // Access and write the rewrite the date ontop of the sreen
+                val Date = ("" + (month + 1) + "-"
+                        + dayOfMonth.toString() + "-" + year)
+                // set this date in TextView for Display
+                dateView!!.text = Date
+            }
+
+        return viewOfLayout
+    }
 }
