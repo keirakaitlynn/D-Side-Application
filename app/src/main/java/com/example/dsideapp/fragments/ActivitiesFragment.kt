4da9d11cb8816d3acc8ec54581a073dff6ec6408
TheGreenThunder <47798163+TheGreenThunder@users.noreply.github.com>
2022-02-10 20:29:25 -0800

    Added rough database functionality to add activities under user. Is triggered when clicking suggestion button.

--- app/src/main/java/com/example/dsideapp/fragments/ActivitiesFragment.kt
+++ app/src/main/java/com/example/dsideapp/fragments/ActivitiesFragment.kt
@@ -12,6 +12,10 @@ import com.example.dsideapp.childfragments.CoinChildFragment
 import com.example.dsideapp.childfragments.DiceChildFragment
 import com.example.dsideapp.childfragments.SuggestionsChildFragment
 import com.example.dsideapp.childfragments.WheelChildFragment
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.auth.FirebaseAuth
 
 class ActivitiesFragment : Fragment() {
     lateinit var suggestionsButton : Button
@@ -24,6 +28,7 @@ class ActivitiesFragment : Fragment() {
     private val diceFragment = DiceChildFragment()
     private val wheelFragment = WheelChildFragment()
 
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,6 +40,18 @@ class ActivitiesFragment : Fragment() {
 
         suggestionsButton = v.findViewById<Button>(R.id.suggestions_button)
         suggestionsButton.setOnClickListener{
+            // Write a message to the database
+            auth = Firebase.auth
+            val database = FirebaseDatabase.getInstance()
+            fun writeNewActivity(userId: String, name: String, location: String, date_time: String) {
+
+                val activity = Activity(name, location, date_time)
+
+                database.reference.child("users").child(userId).child("data").child("activities").setValue(activity)
+
+            }
+            writeNewActivity(auth.uid.toString(), "Party at CSULB","1212 N Bellflower Blvd, Long Beach, CA 90815", "02-10-2022 20:10")
+
             replaceChildFragment(suggestionsFragment)
         }
         coinButton = v.findViewById<Button>(R.id.coin_button)
@@ -56,5 +73,10 @@ class ActivitiesFragment : Fragment() {
     private fun replaceChildFragment(childFragment : Fragment) {
         val transaction: FragmentTransaction = getChildFragmentManager().beginTransaction()
         transaction.replace(R.id.activities_view, childFragment).commit()
+    }
+
+    data class Activity(val username: String? = null, val location: String? = null, val date_time: String? = null) {
+        // Null default values create a no-argument default constructor, which is needed
+        // for deserialization from a DataSnapshot.
     }
 }
