de6e86658e8793f6c28996f423d73e4ceea12329
Estefania Lopez <estefanialopez2018@gmail.com>
2022-02-25 20:50:47 -0800

    Working on daily view (added popup window to fragment_calendar.xml

--- app/src/main/java/com/example/dsideapp/fragments/CalendarFragment.kt
+++ app/src/main/java/com/example/dsideapp/fragments/CalendarFragment.kt
@@ -7,7 +7,21 @@ import android.view.ViewGroup
 import android.widget.CalendarView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import com.example.dsideapp.R
+
+import android.view.Gravity
+
+import android.widget.PopupWindow
+
+import android.widget.LinearLayout
+
+import android.annotation.SuppressLint
+
+import android.view.View.OnTouchListener
+import androidx.core.content.ContextCompat
+
+import androidx.core.content.ContextCompat.getSystemService
+import android.content.Context.LAYOUT_INFLATER_SERVICE as LAYOUT_INFLATER_SERVICE1
+
 
 class CalendarFragment : Fragment() {
     private lateinit var viewOfLayout: View
@@ -20,15 +34,17 @@ class CalendarFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        viewOfLayout = inflater.inflate(R.layout.fragment_calendar, container, false)
+        viewOfLayout = inflater.inflate(com.example.dsideapp.R.layout.fragment_calendar, container, false)
         // Variables for easy manipulation of objects in the activity_main.xml file   🙂
-        calendar = viewOfLayout.findViewById<View>(R.id.calendar) as CalendarView
-        dateView = viewOfLayout.findViewById<View>(R.id.date_view) as TextView
+        calendar = viewOfLayout.findViewById<View>(com.example.dsideapp.R.id.calendar) as CalendarView
+        dateView = viewOfLayout.findViewById<View>(com.example.dsideapp.R.id.date_view) as TextView
 
         // Listener checks for a tap on a day
+
         calendar!!
             .setOnDateChangeListener {
-                // In here we can probably just make a popup / change page with a view of the day and stuff
+
+
                 // and query the DB to actually get the event info 🙂
 
                 _, year, month, dayOfMonth ->
@@ -38,8 +54,36 @@ class CalendarFragment : Fragment() {
                         + dayOfMonth.toString() + "-" + year)
                 // set this date in TextView for Display
                 dateView!!.text = Date
+
+                // In here we can probably just make a popup / change page with a view of the day and stuff
+
+                @SuppressLint("ClickableViewAccessibility")
+                fun onButtonShowPopupWindowClick(view: View?) {
+
+                    // inflate the layout of the popup window
+                    val popupView: View? =
+                        inflater?.inflate(com.example.dsideapp.R.layout.fragment_calendar, null)
+
+                    // create the popup window
+                    val width = LinearLayout.LayoutParams.WRAP_CONTENT
+                    val height = LinearLayout.LayoutParams.WRAP_CONTENT
+                    val focusable = true // lets taps outside the popup also dismiss it
+                    val popupWindow = PopupWindow(popupView, width, height, focusable)
+
+                    // show the popup window
+                    // which view you pass in doesn't matter, it is only used for the window tolken
+                    popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0)
+
+                    // dismiss the popup window when touched
+                    popupView?.setOnTouchListener { v, event ->
+                        popupWindow.dismiss()
+                        true
+                    }
+                }
+
             }
 
         return viewOfLayout
     }
 }
+
