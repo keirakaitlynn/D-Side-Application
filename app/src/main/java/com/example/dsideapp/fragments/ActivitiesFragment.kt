f3ad4e1f0fb4c18853284474f416dc1427e0f235
JV1805 <48598466+JV1805@users.noreply.github.com>
2022-02-25 22:22:49 -0800

    -fixed the cart crash by placing within the doInBackground block -This allows all the needed info to be gathered before the button can be clicked

--- app/src/main/java/com/example/dsideapp/fragments/ActivitiesFragment.kt
+++ app/src/main/java/com/example/dsideapp/fragments/ActivitiesFragment.kt
@@ -1,5 +1,6 @@
 package com.example.dsideapp.fragments
 
+import android.R.attr
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -17,6 +18,7 @@ import android.view.Gravity
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.AsyncTask
+import android.util.Log
 import android.widget.*
 
 import com.example.dsideapp.auth
@@ -29,6 +31,12 @@ import org.jsoup.nodes.Element
 import java.io.IOException
 import java.io.InputStream
 import kotlin.random.Random
+import android.R.attr.button
+
+import android.os.CountDownTimer
+
+
+
 
 class ActivitiesFragment : Fragment() {
     lateinit var suggestionsButton : Button
@@ -98,7 +106,7 @@ class ActivitiesFragment : Fragment() {
                         Jsoup.connect("https://en.wikipedia.org/wiki/Bowling").get()
 
                     //////////
-                    ///Cart image info gathering
+                    ///Info image info gathering
                     //Get the logo source of the website
                     infoImg = infoDocument.getElementsByTag("img").first()!!
                     // Locate the src attribute
@@ -123,7 +131,7 @@ class ActivitiesFragment : Fragment() {
                         //Popup window for the info
                         infoPopUpText = v.findViewById(R.id.popUpTextInfo)
                         if (infoDescription != null) {
-                            infoPopUpText.text = infoDescription.get(1).text().toString()
+                            infoPopUpText.text = infoDescription.get(2).text().toString()
                         }
                         infoImageView = v.findViewById(R.id.popUpImageInfo)
                         //textView = findViewById(R.id.title)
@@ -153,53 +161,50 @@ class ActivitiesFragment : Fragment() {
                     ///
                     //Get the title of the website
                     //title = document.title()
+
+                    /////////////////////////////////////////////////////////////////
+                    //cart pop-up window on button click/
+                    val cartButton = v.findViewById<ImageButton>(R.id.cart_button)
+                    cartButton.setOnClickListener{
+                        // inflate the layout of the popup window
+                        v = inflater.inflate(com.example.dsideapp.R.layout.fragment_cart_pop_up, null)
+                        // create the popup window
+                        val width = LinearLayout.LayoutParams.WRAP_CONTENT
+                        val height = LinearLayout.LayoutParams.WRAP_CONTENT
+                        val focusable = true // lets taps outside the popup also dismiss it
+                        val popupWindow = PopupWindow(v, width, height, focusable)
+
+                        //getting database info
+                        var authorization = auth
+                        var user = authorization.currentUser
+                        var userID = authorization.currentUser?.uid
+                        var db = FirebaseDatabase.getInstance().getReference()
+                        //getting the db info for popup
+                        //var tempTestText = "Activity 1\nActivity 4\nActivity 10\n"
+                        var activityInfo = db.child("users").child(userID.toString()).get().addOnSuccessListener {
+                            //Popup window for the cart
+                            cartPopUpText = v.findViewById(R.id.popUpText)
+                            if (it.exists()){
+                                cartPopUpText.text = it.child("data").child("activities").child("1233abc").child("title").value.toString()
+                                cartImageView = v.findViewById(R.id.popUpImage)
+                                //textView = findViewById(R.id.title)
+                                cartImageView.setImageBitmap(cartBitmap)
+                            }
+                        }
+                        // show the popup window
+                        // which view you pass in doesn't matter, it is only used for the window token
+                        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0)
+                        v.setOnTouchListener { v, event ->
+                            popupWindow.dismiss()
+                            true
+                        }
+
+                    }
                 } catch (e: IOException) {
                     e.printStackTrace()
                 }
                 return null
             }
-
-            override fun onPostExecute(aVoid: Void?) {
-                super.onPostExecute(aVoid)
-            }
-        }
-        /////////////////////////////////////////////////////////////////
-        //cart pop-up window on button click/
-        val cartButton = v.findViewById<ImageButton>(R.id.cart_button)
-        cartButton.setOnClickListener{
-            // inflate the layout of the popup window
-            v = inflater.inflate(com.example.dsideapp.R.layout.fragment_cart_pop_up, null)
-            // create the popup window
-            val width = LinearLayout.LayoutParams.WRAP_CONTENT
-            val height = LinearLayout.LayoutParams.WRAP_CONTENT
-            val focusable = true // lets taps outside the popup also dismiss it
-            val popupWindow = PopupWindow(v, width, height, focusable)
-
-            //getting database info
-            var authorization = auth
-            var user = authorization.currentUser
-            var userID = authorization.currentUser?.uid
-            var db = FirebaseDatabase.getInstance().getReference()
-            //getting the db info for popup
-            //var tempTestText = "Activity 1\nActivity 4\nActivity 10\n"
-            var activityInfo = db.child("users").child(userID.toString()).get().addOnSuccessListener {
-            //Popup window for the cart
-            cartPopUpText = v.findViewById(R.id.popUpText)
-                if (it.exists()){
-                    cartPopUpText.text = it.child("data").child("activities").child("1233abc").child("title").value.toString()
-                    cartImageView = v.findViewById(R.id.popUpImage)
-                    //textView = findViewById(R.id.title)
-                    cartImageView.setImageBitmap(cartBitmap)
-                }
-            }
-            // show the popup window
-            // which view you pass in doesn't matter, it is only used for the window token
-            popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0)
-            v.setOnTouchListener { v, event ->
-                popupWindow.dismiss()
-                true
-            }
-
         }
         WebScratch().execute()
         return v
@@ -209,7 +214,7 @@ class ActivitiesFragment : Fragment() {
         val transaction: FragmentTransaction = getChildFragmentManager().beginTransaction()
         transaction.replace(R.id.activities_view, childFragment).commit()
     }
-//Main
+    //Main
     data class Activity(val username: String? = null, val location: String? = null, val date_time: String? = null) {
         // Null default values create a no-argument default constructor, which is needed
         // for deserialization from a DataSnapshot.
