fd777dc693682c6a4f5ee97623ab07640326280f
Estefania Lopez <estefanialopez2018@gmail.com>
2022-02-26 16:30:54 -0800

    Created Popup window in CalendarFragment.kt and partially worked on UI

--- app/src/main/java/com/example/dsideapp/fragments/CalendarFragment.kt
+++ app/src/main/java/com/example/dsideapp/fragments/CalendarFragment.kt
@@ -28,15 +28,18 @@ class CalendarFragment : Fragment() {
     var calendar: CalendarView? = null
     private var dateView: TextView? = null
 
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreateView(
 
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        viewOfLayout = inflater.inflate(com.example.dsideapp.R.layout.fragment_calendar, container, false)
+        viewOfLayout =
+            inflater.inflate(com.example.dsideapp.R.layout.fragment_calendar, container, false)
         // Variables for easy manipulation of objects in the activity_main.xml file   🙂
-        calendar = viewOfLayout.findViewById<View>(com.example.dsideapp.R.id.calendar) as CalendarView
+        calendar =
+            viewOfLayout.findViewById<View>(com.example.dsideapp.R.id.calendar) as CalendarView
         dateView = viewOfLayout.findViewById<View>(com.example.dsideapp.R.id.date_view) as TextView
 
         // Listener checks for a tap on a day
@@ -47,7 +50,7 @@ class CalendarFragment : Fragment() {
 
                 // and query the DB to actually get the event info 🙂
 
-                _, year, month, dayOfMonth ->
+                    _, year, month, dayOfMonth ->
 
                 // Access and write the rewrite the date ontop of the sreen
                 val Date = ("" + (month + 1) + "-"
@@ -57,33 +60,30 @@ class CalendarFragment : Fragment() {
 
                 // In here we can probably just make a popup / change page with a view of the day and stuff
 
-                @SuppressLint("ClickableViewAccessibility")
-                fun onButtonShowPopupWindowClick(view: View?) {
+                // inflate the layout of the popup window
+                val popupView: View? =
+                    inflater?.inflate(com.example.dsideapp.R.layout.activity_dailyview, null)
 
-                    // inflate the layout of the popup window
-                    val popupView: View? =
-                        inflater?.inflate(com.example.dsideapp.R.layout.fragment_calendar, null)
+                // create the popup window
+                val width = LinearLayout.LayoutParams.WRAP_CONTENT
+                val height = LinearLayout.LayoutParams.WRAP_CONTENT
+                val focusable = true // lets taps outside the popup also dismiss it
+                val popupWindow = PopupWindow(popupView, width, height, focusable)
 
-                    // create the popup window
-                    val width = LinearLayout.LayoutParams.WRAP_CONTENT
-                    val height = LinearLayout.LayoutParams.WRAP_CONTENT
-                    val focusable = true // lets taps outside the popup also dismiss it
-                    val popupWindow = PopupWindow(popupView, width, height, focusable)
+                // show the popup window
+                // which view you pass in doesn't matter, it is only used for the window tolken
+                popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0)
 
-                    // show the popup window
-                    // which view you pass in doesn't matter, it is only used for the window tolken
-                    popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0)
-
-                    // dismiss the popup window when touched
-                    popupView?.setOnTouchListener { v, event ->
-                        popupWindow.dismiss()
-                        true
-                    }
+                // dismiss the popup window when touched
+                popupView?.setOnTouchListener { v, event ->
+                    popupWindow.dismiss()
+                    true
                 }
 
+                // return viewOfLayout
             }
-
         return viewOfLayout
     }
 }
+
 
