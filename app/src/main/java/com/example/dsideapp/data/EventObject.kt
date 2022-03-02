b3cfa28621889dcb04795724c02116fcbb0a5c25
TheGreenThunder <sgkwarp@gmail.com>
2022-03-01 17:17:53 -0800

    Add EventObject to main

---
+++ app/src/main/java/com/example/dsideapp/data/EventObject.kt
@@ -0,0 +1,16 @@
+package com.example.dsideapp.data
+
+import java.util.*
+
+class EventObject (
+    val id: Calendar? = null,
+    val title: Calendar? = null,
+    val phone_contact: ActivityObject? = null,
+    val users: ArrayList<String>? = null) {
+    // Null default values create a no-argument default constructor, which is needed
+    // for deserialization from a DataSnapshot.
+    //Date/Time | Calendar Class - This is something we already have and we definitely should keep to the end. https://developer.android.com/reference/kotlin/android/icu/util/Calendar
+    //End Time | Calendar Class - This will be the end time (End Time - Date/Time = Duration)
+    //Activity | Activity Class - This will be the Activity for the Event
+    //Users | Array of AuthID - These will be participants of event. First participant is Owner
+}
