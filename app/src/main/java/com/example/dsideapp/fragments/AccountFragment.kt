2df9008ffb1fe08e2953bce3ee349a785e6758fc
JV1805 <48598466+JV1805@users.noreply.github.com>
2022-03-05 20:09:04 -0800

    -save username change to database

--- app/src/main/java/com/example/dsideapp/fragments/AccountFragment.kt
+++ app/src/main/java/com/example/dsideapp/fragments/AccountFragment.kt
@@ -4,14 +4,30 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import com.example.dsideapp.R
+import com.example.dsideapp.auth
+import com.google.firebase.database.FirebaseDatabase
 
 class AccountFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_account, container, false)
+    ): View? {
+        var authorization = auth
+        var user = authorization.currentUser
+        var userID = authorization.currentUser?.uid
+        var db = FirebaseDatabase.getInstance().getReference()
 
+        val v = inflater.inflate(R.layout.fragment_account, container, false)
+        var saveChangesButton = v.findViewById<Button>(R.id.changeSaveButton)
+        saveChangesButton.setOnClickListener{
+            db.child("users").child(userID.toString()).child("username")
+                .setValue(v.findViewById<EditText>(R.id.user_name).text.toString())
+        }
+        return v
+    }
 }
