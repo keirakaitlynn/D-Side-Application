173cb2f26d75c9d0da577ef7c973acfecf937de3
Estefania Lopez <estefanialopez2018@gmail.com>
2022-03-15 16:07:16 -0700

    Created popupwindow (need to change the dismissal part)

--- app/src/main/java/com/example/dsideapp/fragments/CalendarFragment.kt
+++ app/src/main/java/com/example/dsideapp/fragments/CalendarFragment.kt
@@ -1,13 +1,16 @@
 package com.example.dsideapp.fragments
 
+import android.content.Context.LAYOUT_INFLATER_SERVICE
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CalendarView
-import android.widget.TextView
+import android.widget.*
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import com.example.dsideapp.R
+
 
 class CalendarFragment : Fragment() {
     private lateinit var viewOfLayout: View
@@ -24,20 +27,52 @@ class CalendarFragment : Fragment() {
         // Variables for easy manipulation of objects in the activity_main.xml file   🙂
         calendar = viewOfLayout.findViewById<View>(R.id.calendar) as CalendarView
         dateView = viewOfLayout.findViewById<View>(R.id.date_view) as TextView
-
+        //var dayOfWeekView =  viewOfLayout.findViewById<View>(R.id.dayOfWeek) as TextView
         // Listener checks for a tap on a day
         calendar!!
             .setOnDateChangeListener {
                 // In here we can probably just make a popup / change page with a view of the day and stuff
                 // and query the DB to actually get the event info 🙂
 
-                _, year, month, dayOfMonth ->
+                    _, year, month, dayOfMonth->
 
                 // Access and write the rewrite the date ontop of the sreen
                 val Date = ("" + (month + 1) + "-"
                         + dayOfMonth.toString() + "-" + year)
                 // set this date in TextView for Display
                 dateView!!.text = Date
+
+                val weekDay = 1;
+
+                //dayOfWeekView!!.text = weekDay
+
+                // In here we can probably just make a popup / change page with a view of the day and stuff
+
+                // inflate the layout of the popup window
+                //val inflater = getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater?
+                val popupView: View? =
+                    inflater?.inflate(com.example.dsideapp.R.layout.activity_dailyview, null)
+
+                // create the popup window
+                val width = LinearLayout.LayoutParams.WRAP_CONTENT
+                val height = LinearLayout.LayoutParams.WRAP_CONTENT
+                val focusable = true // lets taps outside the popup also dismiss it
+                val popupWindow = PopupWindow(popupView, width, height, focusable)
+
+                // show the popup window
+                // which view you pass in doesn't matter, it is only used for the window tolken
+                popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0)
+
+//                var windowButton: Button = viewOfLayout.findViewById(R.id.window) as Button
+//
+//                windowButton.setOnClickListener(View.OnClickListener {
+//                    popupWindow.dismiss()
+//                })
+                 //dismiss the popup window when touched
+                popupView?.setOnTouchListener { v, event ->
+                    popupWindow.dismiss()
+                    true
+                }
             }
 
         return viewOfLayout
