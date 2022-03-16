d6b4a4ce3a7075a76b6a05982620135b48178bd9
★ keira ★ <keirakaitlynn@gmail.com>
2022-03-15 22:58:30 -0700

    Added icons for menu page, polls, invites, and settings. Replaced all instances of "concerts" w/ "polls".

--- app/src/main/java/com/example/dsideapp/fragments/PollsFragment.kt
+++ app/src/main/java/com/example/dsideapp/fragments/PollsFragment.kt
@@ -1,25 +1,20 @@
 package com.example.dsideapp.fragments
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.animation.Animation
-import android.view.animation.LinearInterpolator
-import android.view.animation.RotateAnimation
 import android.widget.*
 import androidx.fragment.app.Fragment
 import com.example.dsideapp.R
-import kotlin.random.Random
 
-class ConcertsFragment : Fragment() {
+class PollsFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? = inflater.inflate(R.layout.fragment_concerts, container, false)
+    ): View? = inflater.inflate(R.layout.fragment_polls, container, false)
 
 
 }
