fe145aa99a5eca2711e5591c0b8444b953c8ca2e
TheGreenThunder <sgkwarp@gmail.com>
2022-03-15 17:49:33 -0700

    PollObject function to calculate percent and lock when

--- app/src/main/java/com/example/dsideapp/data/PollObject.kt
+++ app/src/main/java/com/example/dsideapp/data/PollObject.kt
@@ -1,6 +1,7 @@
 package com.example.dsideapp.data
 
 import java.util.*
+import kotlin.math.max
 
 data class PollObject (
     val id: String? = null,
@@ -9,13 +10,36 @@ data class PollObject (
     val poll_vote_count: MutableList<Int>? = null,
     val poll_end_time: Calendar? = null,
     val business_name: String? = null,
-    val winner_index: Int? = null) {
-    fun calculate_percentages(): //MutableList<Double>
-    //When calculating percentages, replace Boolean with commented out MutableList<Double> and return the resulting list of percentages instead.
-    Boolean {
-        return true
-    }
-    fun send_results(): Boolean {
-        return true
+    var winner_index: Int? = null) {
+    /**
+     * Uses the current poll vote counts to make a list of percents for their votes.
+     * If end time is passed, then set winner_index.
+     * Edge Cases:
+     *  If winner_index is already set, skip functionality.
+     *  If no votes in the poll, skip functionality.
+     * @return
+     */
+    fun calc_perc_n_lock():
+    MutableList<Double> {
+        val curr_votes = this.poll_vote_count!!.sum()
+        val to_return = mutableListOf<Double>()
+        if (winner_index != null) {
+            if (curr_votes != 0) {
+                this.poll_vote_count.forEach {
+                    to_return.add(((it / curr_votes) * 100).toDouble())
+                }
+            } else {
+                System.err.println("There are no votes in this poll. " +
+                            "No calculations are made and function returns empty MutableList")
+            }
+            if (Calendar.getInstance().after(poll_end_time)){
+                winner_index = to_return.indexOf(to_return.maxOrNull() ?: Integer.MAX_VALUE)
+            }
+        }
+        else {
+            System.err.println("Winner is already decided. " +
+                    "No calculations are made and function returns empty MutableList")
+        }
+        return to_return
     }
 }
