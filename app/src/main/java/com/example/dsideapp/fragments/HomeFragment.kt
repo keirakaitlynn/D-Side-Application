6c77b7ce670cc415bc20da1a5f8491bb340cc463
Estefania Lopez <estefanialopez2018@gmail.com>
2022-03-16 21:49:50 -0700

    Added the buttons in the XML file Need to get the icons that were uploaded on the main branch, right now they have the default profile ico

--- app/src/main/java/com/example/dsideapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/dsideapp/fragments/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
@@ -17,23 +18,25 @@ class HomeFragment : Fragment() {
     private val calendarFragment = CalendarFragment()
     private val accountFragment =AccountFragment()
     private val  activitiesFragment= ActivitiesFragment()
-    lateinit var activitiesButton: Button
-    lateinit var calendarButton: Button
+    lateinit var activitiesButton: ImageButton
+    lateinit var calendarButton: ImageButton
     //change to polls in the future
-    lateinit var concertsButton: Button
-    lateinit var infoButton: Button
+    lateinit var concertsButton: ImageButton
+    lateinit var infoButton: ImageButton
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val v = inflater.inflate(com.example.dsideapp.R.layout.fragment_home, container, false)
+       // val v = inflater.inflate(com.example.dsideapp.R.layout.fragment_home, container, false)
+
+        var viewOfLayout = inflater.inflate(R.layout.fragment_home, container, false)
 
         //FragamentTransaction fragmentTrans = getSupport
 
         //add activity button and transaction
-        activitiesButton = v.findViewById<Button>(R.id.suggestions_button)
+        activitiesButton = viewOfLayout.findViewById<View>(R.id.activities_button) as ImageButton
         activitiesButton.setOnClickListener {
             val fragmentManager = getActivity()?.getSupportFragmentManager()
             if (fragmentManager != null) {
@@ -41,7 +44,7 @@ class HomeFragment : Fragment() {
             }
         }
         //add calendar button and transaction
-        calendarButton = v.findViewById<Button>(R.id.suggestions_button)
+        calendarButton = viewOfLayout.findViewById<View>(R.id.calendar_button) as ImageButton
         calendarButton.setOnClickListener {
             val fragmentManager = getActivity()?.getSupportFragmentManager()
             if (fragmentManager != null) {
@@ -49,7 +52,7 @@ class HomeFragment : Fragment() {
             }
         }
         //add info button and transaction
-        infoButton = v.findViewById<Button>(R.id.suggestions_button)
+        infoButton = viewOfLayout.findViewById<View>(R.id.information_button) as ImageButton
         infoButton.setOnClickListener {
             val fragmentManager = getActivity()?.getSupportFragmentManager()
             if (fragmentManager != null) {
@@ -58,7 +61,7 @@ class HomeFragment : Fragment() {
         }
 
         //add concerts(polls) button and transaction
-        concertsButton = v.findViewById<Button>(R.id.suggestions_button)
+        concertsButton = viewOfLayout.findViewById<View>(R.id.concerts_button) as ImageButton
         concertsButton.setOnClickListener {
             val fragmentManager = getActivity()?.getSupportFragmentManager()
             if (fragmentManager != null) {
@@ -74,11 +77,6 @@ class HomeFragment : Fragment() {
 //                fragmentManager.beginTransaction().replace(com.example.dsideapp.R.id.fragment_view, activityTesterFragment).commit()
 //            }
 //        }
-        return v
-    }
-
-    private fun replaceChildFragment(childFragment : Fragment) {
-        val transaction: FragmentTransaction = getChildFragmentManager().beginTransaction()
-        transaction.replace(R.id.activities_view, childFragment).addToBackStack(null).commit()
+        return viewOfLayout
     }
 }
