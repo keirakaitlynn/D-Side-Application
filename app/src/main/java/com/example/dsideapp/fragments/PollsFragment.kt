a90538ef39714eb9a949307d9c35ce49691a4799
Estefania Lopez <estefanialopez2018@gmail.com>
2022-03-20 21:26:54 -0700

    popup

---
+++ app/src/main/java/com/example/dsideapp/fragments/PollsFragment.kt
@@ -0,0 +1,20 @@
+package com.example.dsideapp.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.fragment.app.Fragment
+import com.example.dsideapp.R
+
+class PollsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_polls, container, false)
+
+
+}
