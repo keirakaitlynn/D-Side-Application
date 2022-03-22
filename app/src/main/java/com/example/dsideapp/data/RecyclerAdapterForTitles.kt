de851dd373a01fcc055b1632090a13a27e229e62
JV1805 <48598466+JV1805@users.noreply.github.com>
2022-03-22 16:29:39 -0700

    -Able to vote on polls now

--- app/src/main/java/com/example/dsideapp/data/RecyclerAdapterForTitles.kt
+++ app/src/main/java/com/example/dsideapp/data/RecyclerAdapterForTitles.kt
@@ -1,46 +1,46 @@
 package com.example.dsideapp.data
 
+import android.annotation.SuppressLint
 import android.content.Context
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
+import android.widget.LinearLayout
+import android.widget.PopupWindow
 import androidx.recyclerview.widget.RecyclerView
 import com.example.dsideapp.R
 import com.example.dsideapp.auth
-import com.google.firebase.database.DataSnapshot
+import com.google.android.material.internal.ContextUtils.getActivity
 import com.google.firebase.database.FirebaseDatabase
-import kotlin.random.Random
 
-class RecyclerAdapterForTitles(val context: Context, val titles: MutableList<String>): RecyclerView.Adapter<RecyclerAdapterForTitles.ViewHolder>() {
+class RecyclerAdapterForTitles(val context: Context, val objectPoll: MutableList<PollObject>): RecyclerView.Adapter<RecyclerAdapterForTitles.ViewHolder>() {
 
     // NOTES: size of arrays of info to bind w/ ViewHolder must be equal
 //    private var titles = arrayOf("Chapter One", "Chapter Two", "Chapter Three", "Chapter Five", "Chapter Six", "Chapter Seven", "Chapter Eight")
 //    private var details = arrayOf("Item one details", "Item two details", "Item three details", "Item four details", "Item five details", "Item six details", "Item seven details", "Item eight details")
 //    private var images = intArrayOf(R.drawable.card_1, R.drawable.card_1, R.drawable.card_1, R.drawable.card_1, R.drawable.card_1, R.drawable.card_1, R.drawable.card_1, R.drawable.card_1)
-
+lateinit var  temp: View
+lateinit var temp2: ViewGroup
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
     ): RecyclerAdapterForTitles.ViewHolder {
         // ViewHolder: the layout of the item to be duplicated/"recycled"
-        val v = LayoutInflater.from(parent.context).inflate(R.layout.polls_view, parent, false)
-        return ViewHolder(v)
+        temp = LayoutInflater.from(context).inflate(R.layout.polls_view, parent, false)
+        temp2 = parent
+        return ViewHolder(temp)
     }
 
     override fun getItemCount(): Int {
         // the total # of items you want to display to pass to ViewHolder
-        return titles.size
+        return objectPoll.size
     }
 
     override fun onBindViewHolder(holder: RecyclerAdapterForTitles.ViewHolder, position: Int) {
         // Iterates through an array (cart, images, etc).
         // Binds ViewHolder's vars to RecyclerAdapter's vars (cart_activity_card.xml w/ RecyclerAdapter.kt).
-        val title = titles[position]
-        holder.itemTitle.text = title
-        //holder.bind(title)
+        val title = objectPoll[position]
+        holder.bind(title)
 
         // NOTES: size of arrays of info to bind w/ ViewHolder must be equal
         // NOTES: Line below will crash because images[].size != cart.size (or "getItemCount()")
@@ -48,16 +48,203 @@ class RecyclerAdapterForTitles(val context: Context, val titles: MutableList<Str
     }
 
     inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
-        var itemImage: ImageView
-        var itemTitle: TextView
+        @SuppressLint("RestrictedApi")
+        fun bind(p: PollObject){
+            lateinit var v: View
+            var itemImage: ImageView
+            var itemTitle: Button
+            var layoutThingy = R.layout.fragment_vote2_poll
 
-        /*fun bind(title: String) {
-            itemView.findViewById<Button>(R.id.tvName).text = restaurant.name
-            itemView.findViewById<Button>(R.id.tvName).setOnClickListener{//Voting and deleting here}
-        }*/
-        init {
             itemImage = itemView.findViewById(R.id.user_image)
             itemTitle = itemView.findViewById(R.id.poll_title)
+
+            itemTitle.text = p.business_name
+            //Getting database info
+            var authorization = auth
+            var user = authorization.currentUser
+            var userID = authorization.currentUser?.uid
+            var db = FirebaseDatabase.getInstance().getReference()
+
+            //Giving Voting functionality to the polls
+            itemTitle.setOnClickListener{
+                val numOfOptions = p.poll_options?.size
+                //Setting up popup
+                // inflate the layout of the popup window with correct vote view
+                // create the popup window
+                val width = LinearLayout.LayoutParams.WRAP_CONTENT
+                val height = LinearLayout.LayoutParams.WRAP_CONTENT
+                val focusable = true // lets taps outside the popup also dismiss it
+                //
+                if (numOfOptions == 6){
+                    v = LayoutInflater.from(context).inflate(R.layout.fragment_vote6_poll, null )
+                    layoutThingy = R.layout.fragment_vote6_poll
+                    val b6 = v.findViewById<Button>(R.id.button6)
+                    b6.text = p.poll_options[5]
+                    b6.setOnClickListener{
+                        db.child("Public Polls").child(p.id.toString()).child("opt6Vote")
+                            .setValue(""+(p.poll_vote_count!![5]+1))
+                    }
+                    //
+                    val b5 = v.findViewById<Button>(R.id.button5)
+                    b5.text = p.poll_options[4]
+                    b5.setOnClickListener{
+                        db.child("Public Polls").child(p.id.toString()).child("opt5Vote")
+                            .setValue(""+(p.poll_vote_count!![4]+1))
+                    }
+                    //
+                    val b4 = v.findViewById<Button>(R.id.button4)
+                    b4.text = p.poll_options[3]
+                    b4.setOnClickListener{
+                        db.child("Public Polls").child(p.id.toString()).child("opt4Vote")
+                            .setValue(""+(p.poll_vote_count!![3]+1))
+                    }
+                    //
+                    val b3 = v.findViewById<Button>(R.id.button3)
+                    b3.text = p.poll_options[2]
+                    b3.setOnClickListener{
+                        db.child("Public Polls").child(p.id.toString()).child("opt3Vote")
+                            .setValue(""+(p.poll_vote_count!![2]+1))
+                    }
+                    //
+                    val b2 = v.findViewById<Button>(R.id.button2)
+                    b2.text = p.poll_options[1]
+                    b2.setOnClickListener{
+                        db.child("Public Polls").child(p.id.toString()).child("opt2Vote")
+                            .setValue(""+(p.poll_vote_count!![1]+1))
+                    }
+                    //
+                    val b1 = v.findViewById<Button>(R.id.button)
+                    b1.text = p.poll_options[0]
+                    b1.setOnClickListener{
+                        db.child("Public Polls").child(p.id.toString()).child("opt1Vote")
+                            .setValue(""+(p.poll_vote_count!![0]+1))
+                    }
+                    //
+                }
+                else if (numOfOptions == 5){
+                    v = LayoutInflater.from(context).inflate(R.layout.fragment_vote5_poll, null)
+                    layoutThingy = R.layout.fragment_vote5_poll
+                    val b5 = v.findViewById<Button>(R.id.button5)
+                    b5.text = p.poll_options[4]
+                    b5.setOnClickListener{
+                        db.child("Public Polls").child(p.id.toString()).child("opt5Vote")
+                            .setValue(""+(p.poll_vote_count!![4]+1))
+                    }
+                    //
+                    val b4 = v.findViewById<Button>(R.id.button4)
+                    b4.text = p.poll_options[3]
+                    b4.setOnClickListener{
+                        db.child("Public Polls").child(p.id.toString()).child("opt4Vote")
+                            .setValue(""+(p.poll_vote_count!![3]+1))
+                    }
+                    //
+                    val b3 = v.findViewById<Button>(R.id.button3)
+                    b3.text = p.poll_options[2]
+                    b3.setOnClickListener{
+                        db.child("Public Polls").child(p.id.toString()).child("opt3Vote")
+                            .setValue(""+(p.poll_vote_count!![2]+1))
+                    }
+                    //
+                    val b2 = v.findViewById<Button>(R.id.button2)
+                    b2.text = p.poll_options[1]
+                    b2.setOnClickListener{
+                        db.child("Public Polls").child(p.id.toString()).child("opt2Vote")
+                            .setValue(""+(p.poll_vote_count!![1]+1))
+                    }
+                    //
+                    val b1 = v.findViewById<Button>(R.id.button)
+                    b1.text = p.poll_options[0]
+                    b1.setOnClickListener{
+                        db.child("Public Polls").child(p.id.toString()).child("opt1Vote")
+                            .setValue(""+(p.poll_vote_count!![0]+1))
+                    }
+                    //
+                }
+                else if (numOfOptions == 4){
+                    v = LayoutInflater.from(context).inflate(R.layout.fragment_vote4_poll, null)
+                    layoutThingy = R.layout.fragment_vote4_poll
+                    val b4 = v.findViewById<Button>(R.id.button4)
+                    b4.text = p.poll_options[3]
+                    b4.setOnClickListener{
+                        db.child("Public Polls").child(p.id.toString()).child("opt4Vote")
+                            .setValue(""+(p.poll_vote_count!![3]+1))
+                    }
+                    //
+                    val b3 = v.findViewById<Button>(R.id.button3)
+                    b3.text = p.poll_options[2]
+                    b3.setOnClickListener{
+                        db.child("Public Polls").child(p.id.toString()).child("opt3Vote")
+                            .setValue(""+(p.poll_vote_count!![2]+1))
+                    }
+                    //
+                    val b2 = v.findViewById<Button>(R.id.button2)
+                    b2.text = p.poll_options[1]
+                    b2.setOnClickListener{
+                        db.child("Public Polls").child(p.id.toString()).child("opt2Vote")
+                            .setValue(""+(p.poll_vote_count!![1]+1))
+                    }
+                    //
+                    val b1 = v.findViewById<Button>(R.id.button)
+                    b1.text = p.poll_options[0]
+                    b1.setOnClickListener{
+                        db.child("Public Polls").child(p.id.toString()).child("opt1Vote")
+                            .setValue(""+(p.poll_vote_count!![0]+1))
+                    }
+                    //
+                }
+                else if (numOfOptions == 3){
+                    v = LayoutInflater.from(context).inflate(R.layout.fragment_vote3_poll, null)
+                    layoutThingy = R.layout.fragment_vote3_poll
+                    val b3 = v.findViewById<Button>(R.id.button3)
+                    b3.text = p.poll_options[2]
+                    b3.setOnClickListener{
+                        db.child("Public Polls").child(p.id.toString()).child("opt3Vote")
+                            .setValue(""+(p.poll_vote_count!![2]+1))
+                    }
+                    //
+                    val b2 = v.findViewById<Button>(R.id.button2)
+                    b2.text = p.poll_options[1]
+                    b2.setOnClickListener{
+                        db.child("Public Polls").child(p.id.toString()).child("opt2Vote")
+                            .setValue(""+(p.poll_vote_count!![1]+1))
+                    }
+                    //
+                    val b1 = v.findViewById<Button>(R.id.button)
+                    b1.text = p.poll_options[0]
+                    b1.setOnClickListener{
+                        db.child("Public Polls").child(p.id.toString()).child("opt1Vote")
+                            .setValue(""+(p.poll_vote_count!![0]+1))
+                    }
+                    //
+                }
+                else{
+                    v = LayoutInflater.from(context).inflate(R.layout.fragment_vote2_poll, null)
+                    layoutThingy = R.layout.fragment_vote2_poll
+                    val b2 = v.findViewById<Button>(R.id.button2)
+                    b2.text = p.poll_options!![1]
+                    b2.setOnClickListener{
+                        db.child("Public Polls").child(p.id.toString()).child("opt2Vote")
+                            .setValue(""+(p.poll_vote_count!![1]+1))
+                    }
+                    //
+                    val b1 = v.findViewById<Button>(R.id.button)
+                    b1.text = p.poll_options[0]
+                    b1.setOnClickListener{
+                        db.child("Public Polls").child(p.id.toString()).child("opt1Vote")
+                            .setValue(""+(p.poll_vote_count!![0]+1))
+                    }
+                    //
+                }
+
+
+                var popupView = LayoutInflater.from(getActivity(context)).inflate(layoutThingy, null);
+                var popupWindow = PopupWindow(v, WindowManager.LayoutParams.MATCH_PARENT, WindowManager.LayoutParams.MATCH_PARENT);
+                popupWindow.showAtLocation(popupView, Gravity.CENTER, 0, 0);
+
+//                val popupWindow = PopupWindow(itemView, width, height, focusable)
+                // show the popup window
+//                popupWindow.showAtLocation(temp, Gravity.CENTER, 0, 0)
+            }
         }
     }
 }
--- app/src/main/java/com/example/dsideapp/fragments/PollsFragment.kt
+++ app/src/main/java/com/example/dsideapp/fragments/PollsFragment.kt
@@ -153,15 +153,6 @@ class PollsFragment : Fragment() {
                         val poll_Time: String? = null
                     ) {}
 
-                    var newPoll = PollObject(
-                        pollId,
-                        pollPosterId,
-                        pollOptions,
-                        pollVoteCount,
-                        pollEndTime,
-                        pollTitleInPopUp.text.toString(),
-                        winner_index = 0
-                    )
 
                     var dbReadablePoll = stringPoll(
                         pollTitleInPopUp.text.toString(),
@@ -198,42 +189,73 @@ class PollsFragment : Fragment() {
                 // for loop going through all the active polls
                 val allPolls = it.children
                 val rv = v.findViewById<RecyclerView>(R.id.pollsRecyclerView)
-                val pollViews = mutableListOf<String>()
+                val pollViews = mutableListOf<PollObject>()
                 layoutManager = LinearLayoutManager(requireContext())
                 rv.layoutManager = layoutManager
                 adapter = RecyclerAdapterForTitles(requireContext(), pollViews)
                 rv.adapter = adapter
                 allPolls.forEach { poll ->
                     //Getting poll options from db
-                    val pollOpt1 = poll.child("opt1").value
-                    val pollOpt2 = poll.child("opt2").value
-                    val pollOpt3 = poll.child("opt3").value
-                    val pollOpt4 = poll.child("opt4").value
-                    val pollOpt5 = poll.child("opt5").value
-                    val pollOpt6 = poll.child("opt6").value
+                    val pollOpt1 = poll.child("opt1").value.toString()
+                    val pollOpt2 = poll.child("opt2").value.toString()
+                    val pollOpt3 = poll.child("opt3").value.toString()
+                    val pollOpt4 = poll.child("opt4").value.toString()
+                    val pollOpt5 = poll.child("opt5").value.toString()
+                    val pollOpt6 = poll.child("opt6").value.toString()
+                    //Creating a String List of Options to put in poll object
+                    var tempOptionsHolder = mutableListOf<String>()
+                    tempOptionsHolder.add(pollOpt1)
+                    tempOptionsHolder.add(pollOpt2)
+                    tempOptionsHolder.add(pollOpt3)
+                    tempOptionsHolder.add(pollOpt4)
+                    tempOptionsHolder.add(pollOpt5)
+                    tempOptionsHolder.add(pollOpt6)
 
                     //Getting option votes from db
-                    val pollVoteOpt1 = poll.child("opt1Vote").value
-                    val pollVoteOpt2 = poll.child("opt2Vote").value
-                    val pollVoteOpt3 = poll.child("opt3Vote").value
-                    val pollVoteOpt4 = poll.child("opt4Vote").value
-                    val pollVoteOpt5 = poll.child("opt5Vote").value
-                    val pollVoteOpt6 = poll.child("opt6Vote").value
-                    val pollT = poll.child("poll_TITLE").value
-                    //Getting poll ID from db
-                    var pollId = poll.child("poll_ID").value
-                    //Getting poster ID from db
-                    val pollPosterId = poll.child("poster_ID").value
-                    //Getting poll end time from db
-                    var pollEndTime = poll.child("poll_Time").value
+                    val pollVoteOpt1 = poll.child("opt1Vote").value.toString().toInt()
+                    val pollVoteOpt2 = poll.child("opt2Vote").value.toString().toInt()
+                    val pollVoteOpt3 = poll.child("opt3Vote").value.toString().toInt()
+                    val pollVoteOpt4 = poll.child("opt4Vote").value.toString().toInt()
+                    val pollVoteOpt5 = poll.child("opt5Vote").value.toString().toInt()
+                    val pollVoteOpt6 = poll.child("opt6Vote").value.toString().toInt()
+                    //Creating a Int List of Votes to put in poll object
+                    var tempVoteHolder = mutableListOf<Int>()
+                    tempVoteHolder.add(pollVoteOpt1)
+                    tempVoteHolder.add(pollVoteOpt2)
+                    tempVoteHolder.add(pollVoteOpt3)
+                    tempVoteHolder.add(pollVoteOpt4)
+                    tempVoteHolder.add(pollVoteOpt5)
+                    tempVoteHolder.add(pollVoteOpt6)
 
+
+                    val pollT = poll.child("poll_TITLE").value.toString()
+                    //Getting poll ID from db
+                    var pollId = poll.child("poll_ID").value.toString()
+                    //Getting poster ID from db
+                    val pollPosterId = poll.child("poster_ID").value.toString()
+                    //Getting poll end time from db
+                    var pollEndTime = poll.child("poll_Time").value.toString().toInt()
+                    //Getting poll name
+                    var pollName = poll.child("poll_TITLE").value.toString()
+                    //Creating the poll object
+                    var newPoll = PollObject(
+                        pollId,
+                        pollPosterId,
+                        tempOptionsHolder,
+                        tempVoteHolder,
+                        pollEndTime,
+                        pollName,
+                        winner_index = 0
+                    )
                     //Adding poll to recycler view
-                    pollViews.add(pollT.toString())
+                    //put in the poll object instead of only the title
+                    pollViews.add(newPoll)
                     //Log.w("NOT SURE: ", pollId.toString())
                 }
             }
         }
         adapter?.notifyDataSetChanged()
+
         return v
     }
     private fun replaceChildFragment(childFragment : Fragment) {
