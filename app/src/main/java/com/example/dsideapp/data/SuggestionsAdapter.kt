b7ce3f5cbc57eba5020b7a0fa723e89af68214da
★ keira ★ <keirakaitlynn@gmail.com>
2022-03-26 18:07:09 -0700

    Data-Bind Yelp API data to Suggestions Fragment.

---
+++ app/src/main/java/com/example/dsideapp/data/SuggestionsAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.dsideapp.data
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+
+class SuggestionsAdapter(context: Context?, strings: MutableList<YelpRestaurant>) :
+    ArrayAdapter<YelpRestaurant?>(context!!, 0, strings as List<YelpRestaurant?>) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var convertView: View? = convertView
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(com.example.dsideapp.R.layout.activity_card, parent, false);
+        }
+
+        val restaurant = getItem(position)
+
+        val activityTitle = convertView?.findViewById(com.example.dsideapp.R.id.activity_title) as TextView
+        val activityCategory = convertView.findViewById(com.example.dsideapp.R.id.activity_category) as TextView
+        val activityRadius = convertView.findViewById(com.example.dsideapp.R.id.activity_radius) as TextView
+        val firstCategory = restaurant?.categories?.get(0)
+        activityTitle.setText(restaurant?.name)
+        activityCategory.setText(firstCategory?.title)
+        activityRadius.setText(restaurant?.displayDistance())
+
+        return convertView
+    }
+}
