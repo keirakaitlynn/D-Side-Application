86d6dd5555dfe746d179c22cc6fe6af55fa567c3
TheGreenThunder <47798163+TheGreenThunder@users.noreply.github.com>
2022-04-03 20:33:24 -0700

    Added info button on AccountFragment. Although, the button doesn't work :( "    Process: com.example.dsideapp, PID: 6476     java.lang.IllegalArgumentException: No view found for id 0x7f090055 (com.example.dsideapp:id/activities_view) for fragment InformationChildFragment{f112930} (3ca7036e-29c6-4b52-ac94-a1bb34922d52 id=0x7f090055)" Doesn't point to any particular line.

--- app/src/main/java/com/example/dsideapp/childfragments/InformationChildFragment.kt
+++ app/src/main/java/com/example/dsideapp/childfragments/InformationChildFragment.kt
@@ -6,27 +6,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageButton
 import androidx.fragment.app.FragmentTransaction
 import com.example.dsideapp.R
 
 class InformationChildFragment : Fragment() {
+    lateinit var teamButton : Button
+    lateinit var FAQButton : Button
 
     private val teamFragment = TeamChildFragment()
     private val FAQFragment = FAQChildFragment()
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         var v = inflater.inflate(R.layout.fragment_information_child, container, false)
 
-        val teamButton = v.findViewById<Button>(R.id.ToTeamPage)
+        teamButton = v.findViewById<Button>(R.id.ToTeamPage)
         teamButton.setOnClickListener{
             replaceChildFragment(teamFragment)
         }
 
-        val FAQButton = v.findViewById<Button>(R.id.ToFAQPage)
+        FAQButton = v.findViewById<Button>(R.id.ToFAQPage)
         FAQButton.setOnClickListener{
             replaceChildFragment(FAQFragment)
         }
