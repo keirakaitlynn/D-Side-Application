96f4852210778bc4bf24a3e1e2c79e5f255b0b0b
Victor Rodriguez <victor.rodriguez02@student.csulb.edu>
2022-04-14 16:57:58 -0700

    My latest edits and stuffs

--- app/src/main/java/com/example/dsideapp/data/EventObject.kt
+++ app/src/main/java/com/example/dsideapp/data/EventObject.kt
@@ -2,15 +2,30 @@ package com.example.dsideapp.data
 
 import java.util.*
 
-class EventObject (
-    val id: Calendar? = null,
-    val title: Calendar? = null,
-    val phone_contact: ActivityObject? = null,
-    val users: ArrayList<String>? = null) {
+data class EventObject (
+    var event_title: String? = null,
+    var start_time: Calendar? = null,
+    var end_time: Calendar? = null,
+    var activity: ActivityObject? = null,
+    var users: MutableList<String>? = null,
+    var checked: Boolean = false) {
     // Null default values create a no-argument default constructor, which is needed
     // for deserialization from a DataSnapshot.
     //Date/Time | Calendar Class - This is something we already have and we definitely should keep to the end. https://developer.android.com/reference/kotlin/android/icu/util/Calendar
     //End Time | Calendar Class - This will be the end time (End Time - Date/Time = Duration)
     //Activity | Activity Class - This will be the Activity for the Event
     //Users | Array of AuthID - These will be participants of event. First participant is Owner
+    //Checked | Boolean - If the user already gave feedback on this event.
+
+
+    // Concerts an Event Object to an Activity Object.
+    //  Actually returns a new Activity Object from the original.
+    fun EventObject.toActivityObject()  = ActivityObject(
+        id = this.activity!!.id,
+        title = this.activity!!.title,
+        phone_contact = this.activity!!.phone_contact,
+        location = this.activity!!.location,
+        business_name = this.activity!!.business_name,
+        price = this.activity!!.price
+    )
 }
--- app/src/main/java/com/example/dsideapp/fragments/AccountFragment.kt
+++ app/src/main/java/com/example/dsideapp/fragments/AccountFragment.kt
@@ -1,17 +1,24 @@
 package com.example.dsideapp.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.EditText
+import android.widget.*
 import androidx.fragment.app.Fragment
 import com.example.dsideapp.R
 import com.example.dsideapp.auth
+import com.example.dsideapp.data.FriendClass
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
 
 class AccountFragment : Fragment() {
+    private lateinit var listView  : ListView
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,13 +29,130 @@ class AccountFragment : Fragment() {
         var user = authorization.currentUser
         var userID = authorization.currentUser?.uid
         var db = FirebaseDatabase.getInstance().getReference()
-
         var v = inflater.inflate(R.layout.fragment_account, container, false)
+        var user_DB_Name = ""
+        var user_DB_UserName = ""
+        var user_DB_Location = ""
+
+        // Goes through the children of the DB and saves their variables to the user_DB_ variables
+        // So we can check them with the user's new variables that they type in.
+        db.child("users").child(userID.toString()).addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    val value = dataSnapshot.children
+                    for (x in value){
+                        // This would be the data child, and we aren't working with that here
+                        if (x.hasChildren()){
+                        }
+                        else{
+                            if(x.key.toString() == "location"){
+                                Log.w("XD", "displayed location")
+                                user_DB_Location = x.value.toString()
+                                v.findViewById<EditText>(R.id.user_location).setText(x.value.toString())
+                            }
+                            else if(x.key.toString() == "realName"){
+                                Log.w("XD", "displayed name")
+                                user_DB_Name = x.value.toString()
+                                v.findViewById<EditText>(R.id.user_realName).setText(x.value.toString())
+                            }
+                            else if(x.key.toString() == "UserName"){
+                                Log.w("XD", "displayed USERname")
+                                user_DB_UserName = x.value.toString()
+                                v.findViewById<EditText>(R.id.user_name).setText(x.value.toString())
+                            }
+
+                            else if(x.key.toString() == "favorite_Activities"){
+                                listView = v.findViewById<ListView>(R.id.favorite_activities_list)
+                                var favorite_Activities = MutableList<String>(0){""}
+                                var current = 0
+                                var currentFavoriteActivity = ""
+                                for( i in x.key.toString().split(",") ){
+                                    current += 1
+                                    if(current % 2 == 0){
+                                        currentFavoriteActivity +=   " : "+ i
+
+                                        favorite_Activities.add(currentFavoriteActivity)
+                                        currentFavoriteActivity = ""
+                                    }
+                                    else {
+                                        currentFavoriteActivity += i
+
+                                    }
+                                }
+                                // Adds an adapter to actually display the info on the page.
+                                var listAdapter =
+                                    context?.let { ArrayAdapter<String>(it, R.layout.favorite_activities_in_accounts, R.id.itemTextView,favorite_Activities) }
+                                listView.adapter = listAdapter
+                            }
+                        }
+                    }
+
+                }
+                override fun onCancelled(databaseError: DatabaseError) {}
+            })
+
+
         var saveChangesButton = v.findViewById<Button>(R.id.changesSaveButton)
+        var currentParam : String = ""
+
+        // Goes through every text field, checks that the field isn't empty & that the user's values
+        // aren't the same as before, then writes to db :)
         saveChangesButton.setOnClickListener{
-            db.child("users").child(userID.toString()).child("username")
-                .setValue(v.findViewById<EditText>(R.id.user_name).text.toString())
+
+            currentParam = v.findViewById<EditText>(R.id.user_realName).text.toString()
+            if(currentParam.isNotEmpty() && currentParam != user_DB_Name){
+                Log.w("XD", "Updated the user's name")
+                db.child("users").child(userID.toString()).child("realName")
+                    .setValue(currentParam)
+            }
+            currentParam = v.findViewById<EditText>(R.id.user_location).text.toString()
+            if(currentParam.isNotEmpty() && currentParam != user_DB_Location){
+                Log.w("XD", "Updated the user's location")
+                db.child("users").child(userID.toString()).child("location")
+                    .setValue(currentParam)
+            }
+
+            currentParam = v.findViewById<EditText>(R.id.user_name).text.toString()
+            if(currentParam.isNotEmpty() && currentParam != user_DB_UserName){
+                Log.w("XD", "Updated the user's userName")
+                db.child("users").child(userID.toString()).child("UserName")
+                    .setValue(currentParam)
+            }
+
+            ///                 Might need to do this one differently because I can't get their passwords :(
+            //                  It might be a good idea to make it so they have to check a box before having
+            //                  The ability to tap onto the EditText and then change their password.
+            currentParam = v.findViewById<EditText>(R.id.user_password).text.toString()
+            if(currentParam.isNotEmpty()){
+                user?.updatePassword(currentParam)?.addOnSuccessListener {
+                    Log.w("XD", "The pass should have changed")
+                }
+            }
+
         }
+
+        // Reads and displays the user's info on the Account Fragment under friends.
+        db.child("users").child(auth.uid.toString()).child("data").child("Friends").addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // Converts all children (data under Friends) into FriendClass
+                val list = dataSnapshot.children.map{ it.getValue(FriendClass :: class.java)!!}
+                // Saves their usernames to the List to display in the ListView
+                var friends = MutableList<String>(0){""}
+                for (x in list){
+                    friends.add(x.userName.toString())
+                    Log.w("THE", x.userName.toString())
+                }
+                listView = v.findViewById<ListView>(R.id.friends_list)
+
+                // Adds an adapter to actually display the info on the page.
+                var listAdapter =
+                    context?.let { ArrayAdapter<String>(it, R.layout.friend_view, R.id.itemTextView,friends) }
+                listView.adapter = listAdapter
+
+            }
+            override fun onCancelled(databaseError: DatabaseError) {}
+        })
+        v.findViewById<EditText>(R.id.friends_list)
+
         return v
     }
 }
