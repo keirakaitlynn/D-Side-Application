0901c07a8a751e83463fd3de531b3d11653b7d47
★ keira ★ <keirakaitlynn@gmail.com>
2022-04-19 21:51:20 -0700

    Updated Activity & Event Objects w/ time as Dates.

--- app/src/main/java/com/example/dsideapp/data/EventObject.kt
+++ app/src/main/java/com/example/dsideapp/data/EventObject.kt
@@ -2,13 +2,20 @@ package com.example.dsideapp.data
 
 import java.util.*
 
+//data class EventObject (
+//    var event_title: String? = null,
+//    var start_time: Calendar? = null,
+//    var end_time: Calendar? = null,
+//    var activity: ActivityObject? = null,
+//    var users: MutableList<String>? = null,
+//    var checked: Boolean = false) {
 data class EventObject (
+    var id: String? = null,
     var event_title: String? = null,
-    var start_time: Calendar? = null,
-    var end_time: Calendar? = null,
+    var start_time: Date? = null,
+    var end_time: Date? = null,
     var activity: ActivityObject? = null,
-    var users: MutableList<String>? = null,
-    var checked: Boolean = false) {
+    var users: MutableList<String>? = null) {
     // Null default values create a no-argument default constructor, which is needed
     // for deserialization from a DataSnapshot.
     //Date/Time | Calendar Class - This is something we already have and we definitely should keep to the end. https://developer.android.com/reference/kotlin/android/icu/util/Calendar
@@ -16,7 +23,6 @@ data class EventObject (
     //Activity | Activity Class - This will be the Activity for the Event
     //Users | Array of AuthID - These will be participants of event. First participant is Owner
     //Checked | Boolean - If the user already gave feedback on this event.
-
 
     // Concerts an Event Object to an Activity Object.
     //  Actually returns a new Activity Object from the original.
